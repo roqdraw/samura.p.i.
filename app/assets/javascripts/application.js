b1c76ce8562b841651b9557b72366afd933a1434
roqdraw <roqdraw@gmail.com>
2019-04-02 22:27:03 +1100

    made changes to files - big change: httparty instead of fetch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,3 @@
 $(document).ready(function(){
     console.log("jQuery READY")
 })
-
--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,7 +1,16 @@
 class MediaController < ApplicationController
   def index
-    # @result = HTTParty.get('https://api.themoviedb.org/3/search/movie?api_key=2e4bf3ae7a9007a8ff8ac9fbfdeb67d5&language=en-US&query=samurai&page=1&include_adult=true');
-    # @list = @result.body
+    movies_result = HTTParty.get('https://api.themoviedb.org/3/search/movie?api_key=2e4bf3ae7a9007a8ff8ac9fbfdeb67d5&language=en-US&query=samurai&page=1&include_adult=true')
+    movies_list = JSON.parse(movies_result.to_s)
+    @movies = movies_list['results']
+
+    tvshows_result = HTTParty.get('https://api.themoviedb.org/3/search/tv?api_key=2e4bf3ae7a9007a8ff8ac9fbfdeb67d5&language=en-US&query=samurai&page=1')
+    tvshows_list = JSON.parse(tvshows_result.to_s)
+    @tvshows = tvshows_list['results']
+
+    books_result = HTTParty.get('https://www.googleapis.com/books/v1/volumes?q=samurai&printType=books&maxResults=15&key=AIzaSyAwLGxNQqntI0-yaJ5DeFbudX_DrugiUK8')
+    books_list = JSON.parse(books_result.to_s)
+    @books = books_list['items']
   end
 
   def new
