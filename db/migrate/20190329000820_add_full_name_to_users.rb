b5895a18bcae18f543fa0ac7b525f13eb83c9bad
roqdraw <roqdraw@gmail.com>
2019-04-02 15:00:28 +1100

    remove obselete migrations that were causing migration issues on production

--- db/migrate/20190329000820_add_full_name_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFullNameToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :first_name, :text
-    add_column :users, :last_name, :text
-  end
-end
--- db/migrate/20190329022501_rename_password_digest_as_password_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenamePasswordDigestAsPasswordToUsers < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :password_digest, :password
-  end
-end
